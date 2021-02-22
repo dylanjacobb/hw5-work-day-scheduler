af6cd3aebdea9655ed7d9da2aa58d3dbaa15a9c4
Dylan Jacob <dylanjacob.re@gmail.com>
2021-02-21 22:11:26 -0500

    initial commit

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+var timeBlock = document.querySelector(".time-block")
+// var hours = ["09:00 AM", "10:00 AM ", "11:00 AM ", "12:00 PM ", "01:00 PM", "02:00 PM", "03:00 PM", "04:00 PM", "05:00 PM"];
+var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+// create a for loop for the hours
+
+
+
+
